33b522c632ffc4b23a2c9fa69ab8e65bfb490ce2
nyobabot <97266522+nyobabot@users.noreply.github.com>
2022-01-17 19:57:41 +0700

    Update main.js

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ global.timestamp = {
   start: new Date
 }
 // global.LOGGER = logs()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8000
 global.opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
 
 global.prefix = new RegExp('^[' + (opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
